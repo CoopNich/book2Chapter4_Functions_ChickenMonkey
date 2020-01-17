d38d870ade736e255fdfe4aa6000ea9c8c5aceef
Cooper Nichols <coopnich92@gmail.com>
2020-01-17 15:05:46 -0600

    concise_commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,14 @@
 const numberCount = () => {
 
     for (i = 1; i <= 100; i++) {
-        if ((i % 5 !== 0) && (i % 7 !== 0)) {
-            console.log(i)
-        }
-        if (i % 5 === 0) {
-            console.log("Chicken")
-        }
-        if (i % 7 === 0) {
-            console.log("Monkey")
-        }
-        if ((i % 5 === 0) && (i % 7 === 0)) {
+        if (i % 5 === 0 && i % 7 === 0) {
             console.log("ChickenMonkey")
+        } else if (i % 5 === 0) {
+            console.log("Chicken")
+        } else if (i % 7 === 0) {
+            console.log("Monkey")
+        } else {
+            console.log(i)
         }
 
     }
